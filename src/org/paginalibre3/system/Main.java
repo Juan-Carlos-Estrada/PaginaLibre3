58971906d2dcdca1c17eb1afff2c1cc433fef13f
JuanCarlos <jestrada-2026061@kinal.edu.gt>
2026-09-01 22:25:29 -0600

    T0.13 Feat(main): Se agrego la configuración de arranque de JavaFX

--- src/org/paginalibre3/system/Main.java
+++ src/org/paginalibre3/system/Main.java
@@ -2,11 +2,25 @@
 package org.paginalibre3.system;
 
 
-public class Main {
-
-
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+ 
+public class Main extends Application {
+ 
+    @Override
+    public void start(Stage stage) {
+        VBox root = new VBox(15);
+        root.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(root, 600, 500);
+        stage.setTitle("PLTRES");
+        stage.setScene(scene);
+        stage.show();
+    }
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        launch(args);
     }
     
 }
