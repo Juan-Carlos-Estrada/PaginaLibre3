e3e5cc042dccc5336c9fae6784361f69dda34804
Joaquin Sanchez <jsanchez-2026117@kinal.edu.gt>
2026-09-02 23:27:19 -0600

    T1.12 feat: Se implemento alta de usuario en usuarioFXXontrller

--- src/org/paginalibre3/dao/UsuarioDAO.java
+++ src/org/paginalibre3/dao/UsuarioDAO.java
@@ -1,4 +1,4 @@
-package org.paginalibre3.dao;
+package org.paginalib3.dao;
 
 import org.paginalib3.model.Usuario;
 import java.sql.SQLException;
@@ -7,11 +7,58 @@ import java.sql.ResultSet;
 import java.sql.CallableStatement;
 import java.util.ArrayList;
 import java.util.List;
-import org.paginalibre3.util.Conexion;
+import org.paginalib3.util.Conexion;
 
 public class UsuarioDao {
 
-       public List<Usuario> listarTodos() {
+    public Usuario iniciarSesion(String username, String passwordHash) {
+        Usuario usuario = null;
+        String sql = "{call sp_iniciar_sesion(?, ?)}";
+
+        try(Connection conexion = Conexion.getInstancia().conectar();
+                CallableStatement consultaCall = conexion.prepareCall(sql)) {
+
+            consultaCall.setString(1, username);
+            consultaCall.setString(2, passwordHash);
+
+            try(ResultSet tablaResultado = consultaCall.executeQuery()) {
+                if (tablaResultado.next()) {
+                    usuario = new Usuario();
+                    usuario.setId(tablaResultado.getInt(1));
+                    usuario.setUsrname(tablaResultado.getString(2));
+                    usuario.setRol(tablaResultado.getString(3));
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Error en iniciar sesion: " + e.getMessage());
+        }
+
+        return usuario;
+    }
+
+    public boolean registrarUsuario(String username, String password, String rol) {
+        boolean registroExitoso = false;
+        String sql = "{call sp_registrar_usuario(?, ?, ?)}";
+
+        try(Connection conexion = Conexion.getInstancia().conectar();
+                CallableStatement consultaCall = conexion.prepareCall(sql)) {
+
+            consultaCall.setString(1, username);
+            consultaCall.setString(2, password);
+            consultaCall.setString(3, rol);
+
+            int filasAfectadas = consultaCall.executeUpdate();
+            if (filasAfectadas > 0) {
+                registroExitoso = true;
+            }
+        } catch (SQLException e) {
+            System.err.println("Error al registrar usuario: " + e.getMessage());
+        }
+
+        return registroExitoso;
+    }
+
+    public List<Usuario> listarTodos() {
         List<Usuario> listaUsuarios = new ArrayList<>();
         String sql = "{call sp_listar_usuarios()}";
 
@@ -53,9 +100,3 @@ public class UsuarioDao {
                     );
                 }
             }
-        } catch (SQLException e) {
-            System.err.println("Error al buscar usuario: " + e.getMessage());
-        }
-
-        return usuario;
-    }
