8c904790505b9ae61f0ceb2a9b6b313cc88b1974
Angel Molina <amolina-2026053@kinal.edu.gt>
2026-09-02 21:06:06 -0600

    T1.25 feat(ft/US1.4-cambio-password-D): se actualizo el hash en SecurityUtil

--- src/org/paginalibre3/model/Usuario.java
+++ src/org/paginalibre3/model/Usuario.java
@@ -1,13 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.paginalibre3.model;
 
-/**
- *
- * @author Usuario
- */
 public class Usuario {
-    
+
+    private int id;
+    private String usrname;
+    private String rol;
+    private boolean activo;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, String usrname, String rol) {
+        this.id = id;
+        this.usrname = usrname;
+        this.rol = rol;
+    }
+
+    public Usuario(int id, String usrname, String rol, boolean activo) {
+        this.id = id;
+        this.usrname = usrname;
+        this.rol = rol;
+        this.activo = activo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsrname() {
+        return usrname;
+    }
+
+    public void setUsrname(String usrname) {
+        this.usrname = usrname;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
 }
